23ae101caeef83af54b4d23ca9db4afac7b81572
Jint3x <jintexgaming@gmail.com>
2020-10-10 04:50:49 +0300

    Multiple Claims Added

--- scripts/claims.js
+++ scripts/claims.js
@@ -13,7 +13,44 @@ let userData = {
         X: -381,
         Z: 1194,
         desc: "300x300 spawn claim"
-    }
+    },
+    claim: {
+        username: "Popdey",
+        X: 967,
+        Z: 331,
+        desc: "Don't build near"
+    },
+    claim: {
+        username: "FakeFrede",
+        X: 3600,
+        Z: 1900,
+        desc: "Don't build near"
+    },
+    claim: {
+        username: "Emorgancy",
+        X: -2188,
+        Z: -287,
+        desc: "Don't build near"
+    },
+    claim: {
+        username: "Gamecrazybro/NewtStock/C4",
+        X: 170,
+        Z: -780,
+        desc: "Don't build near"
+    },
+    claim: {
+        username: "DigitalRabbit",
+        X: -2182,
+        Z: -1003,
+        desc: "Don't build near"
+    },
+    claim: {
+        username: "_Portal_",
+        X: 463,
+        Z: 187,
+        desc: "Don't build near"
+    },
+    
 };
 
 
